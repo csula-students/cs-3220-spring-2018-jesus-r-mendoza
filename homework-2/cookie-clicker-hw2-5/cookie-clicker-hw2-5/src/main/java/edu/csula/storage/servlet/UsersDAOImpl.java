9d90aeeb73ef1aa8f9cb989b6ab723e2dc060715
jesus-r-mendoza <jesus-r-mendoza@yahoo.com>
2018-04-20 18:45:06 -0700

    finished and passed UserDao tests

--- homework-2/cookie-clicker-hw2-5/cookie-clicker-hw2-5/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
+++ homework-2/cookie-clicker-hw2-5/cookie-clicker-hw2-5/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
@@ -24,17 +24,26 @@ public class UsersDAOImpl implements UsersDAO {
 	public boolean authenticate(String username, String password) {
 		// TODO: check if username/password combination is valid and store the
 		//       username/password into the session
+		if ( username.equals("admin") && password.equals("cs3220password") ) {
+			User user = new User(0, username, password);
+			context.setAttribute(CONTEXT_NAME, user);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public Optional<User> getAuthenticatedUser() {
 		// TODO: return the authenticated user if there is any
+		User user = (User) context.getAttribute(CONTEXT_NAME);
+		if ( user != null)
+			return Optional.of(user);
 		return Optional.empty();
 	}
 
 	@Override
 	public void logout() {
-		// TOOD: log user out using `invalidate`
+		// TODO: log user out using `invalidate`
+		context.invalidate();
 	}
 }
