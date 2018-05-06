e8598ef53e969e0fad8a344d2a4af464a4a972a5
jesus-r-mendoza <jesus-r-mendoza@yahoo.com>
2018-05-05 20:02:02 -0700

    passed first test , working on third

--- lab07/cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
+++ lab07/cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
@@ -1,5 +1,10 @@
 package edu.csula.storage.mysql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -13,8 +18,8 @@ public class EventsDAOImpl implements EventsDAO {
 
 	// TODO: fill the Strings with the SQL queries as "prepated statements" and
 	//       use these queries variable accordingly in the method below
-	protected static final String getAllQuery = "";
-	protected static final String getByIdQuery = "";
+	protected static final String getAllQuery = "SELECT * FROM Events;";
+	protected static final String getByIdQuery = "SELECT * FROM Events WHERE id = ?;";
 	protected static final String setQuery = "";
 	protected static final String addQuery = "";
 	protected static final String removeQuery = "";
@@ -26,12 +31,40 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public List<Event> getAll() {
 		// TODO: get all events from jdbc
+				
+		try {
+			ArrayList<Event> events;			
+			Connection c = context.getConnection();
+			Statement stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery(getAllQuery);
+			events = new ArrayList<>();
+			while(rs.next()) {
+				Event e = new Event(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getInt("trigger_at"));
+				events.add(e);
+				return events;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		}
 		return new ArrayList<>();
 	}
 
 	@Override
 	public Optional<Event> getById(int id) {
-		// TODO: get specific event by id
+		try {
+			
+			Connection c = context.getConnection();
+			PreparedStatement pstmt = c.prepareStatement(getByIdQuery);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery(getByIdQuery);
+			rs.next();
+			Event e = new Event(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getInt("trigger_at"));
+			return Optional.of(e);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return Optional.empty();
 	}
 
