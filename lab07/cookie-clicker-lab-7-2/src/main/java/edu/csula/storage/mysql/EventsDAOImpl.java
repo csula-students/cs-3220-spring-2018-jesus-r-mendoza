cc1c3a23891b1c6b6d6ee6bd8085eb8d1655c536
jesus-r-mendoza <jesus-r-mendoza@yahoo.com>
2018-05-06 22:04:50 -0700

    still only passed one test

--- lab07/cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
+++ lab07/cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.ServletException;
+
 import edu.csula.storage.EventsDAO;
 import edu.csula.storage.Database;
 import edu.csula.models.Event;
@@ -21,7 +23,7 @@ public class EventsDAOImpl implements EventsDAO {
 	protected static final String getAllQuery = "SELECT * FROM Events;";
 	protected static final String getByIdQuery = "SELECT * FROM Events WHERE id = ?;";
 	protected static final String setQuery = "";
-	protected static final String addQuery = "";
+	protected static final String addQuery = "INSERT INTO Events VALUES ( ?, ?, ?, ?);";
 	protected static final String removeQuery = "";
 
 	public EventsDAOImpl(Database context) {
@@ -31,33 +33,51 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public List<Event> getAll() {
 		// TODO: get all events from jdbc
-				
-		try {
+		
+		try ( Connection connection = context.getConnection(); Statement statement = connection.createStatement(); ) {
 			ArrayList<Event> events;			
-			Connection c = context.getConnection();
-			Statement stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery(getAllQuery);
+			ResultSet rs = statement.executeQuery(getAllQuery);
 			events = new ArrayList<>();
 			while(rs.next()) {
 				Event e = new Event(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getInt("trigger_at"));
 				events.add(e);
-				return events;
 			}
+			return events;
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
 		}
 		return new ArrayList<>();
 	}
 
 	@Override
 	public Optional<Event> getById(int id) {
+		System.out.println(id + " -- before");
+		try ( Connection connection = context.getConnection(); PreparedStatement statement = connection.prepareStatement(getByIdQuery); ) {
+			statement.setInt(1, id);
+			ResultSet rs = statement.executeQuery();
+			boolean val = rs.next();
+			System.out.println(val);
+			if( val ) {
+				System.out.println(id + " -- right before");
+				Event e = new Event(id, rs.getString("name"), rs.getString("description"), rs.getInt("trigger_at"));
+				System.out.println(id + " -- right after");
+				System.out.println("retrieved values -- " + rs.getInt("id") + ", " + rs.getString("name") + ", " + rs.getString("description") + ", " + rs.getInt("trigger_at"));				
+				System.out.println("retrieved values -- " + e.getId() + ", " + e.getName() + ", " + e.getDescription() + ", " + e.getTriggerAt() );				
+				return Optional.of(e);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		/*
 		try {
 			
 			Connection c = context.getConnection();
 			PreparedStatement pstmt = c.prepareStatement(getByIdQuery);
 			pstmt.setInt(1, id);
-			ResultSet rs = pstmt.executeQuery(getByIdQuery);
+			ResultSet rs = pstmt.executeUpdate();
 			rs.next();
 			Event e = new Event(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getInt("trigger_at"));
 			return Optional.of(e);
@@ -65,6 +85,7 @@ public class EventsDAOImpl implements EventsDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		*/
 		return Optional.empty();
 	}
 
@@ -76,6 +97,15 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public void add(Event event) {
 		// TODO: implement jdbc logic to add a new event
+		try (Connection c = context.getConnection(); PreparedStatement stmt = c.prepareStatement(addQuery)) {
+			stmt.setInt(1, event.getId());
+			stmt.setString(2, event.getName());
+			stmt.setString(3, event.getDescription());
+			stmt.setInt(4, event.getTriggerAt());
+			stmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
